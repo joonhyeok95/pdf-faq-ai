f71b0a9dce45d2358418a7fea38a732ac178481b
joonhyeok95 <dkttkemf@gmail.com>
2026-03-05 16:42:29 +0900

    PGvector warning 처리(라이브러리변경)
    langchain_community 를 langchain_postgres 로 변경하고 적용

--- core/database.py
+++ core/database.py
@@ -1,5 +1,6 @@
 import os
-from langchain_community.vectorstores import PGVector
+# from langchain_community.vectorstores import PGVector # deprecated..
+from langchain_postgres.vectorstores import PGVector as PGVectorStore # 별칭 사용 가능
 from dotenv import load_dotenv
 
 # 모듈 단위에서도 환경 변수를 로드할 수 있도록 추가
@@ -9,19 +10,19 @@ COLLECTION_NAME = "streamlit_pdf_rag"
 CONNECTION_STRING = os.getenv("PGVECTOR_CONNECTION_STRING")
 
 def get_vector_db(embeddings):
-    return PGVector(
-        connection_string=CONNECTION_STRING,
+    return PGVectorStore(
+        connection=CONNECTION_STRING,
         collection_name=COLLECTION_NAME,
-        embedding_function=embeddings,
+        embeddings=embeddings,
         use_jsonb=True
     )
 
 def save_to_vector_db(docs, embeddings):
-    PGVector.from_documents(
+    PGVectorStore.from_documents(
         embedding=embeddings,
         documents=docs,
         collection_name=COLLECTION_NAME,
-        connection_string=CONNECTION_STRING,
+        connection=CONNECTION_STRING,
         pre_delete_collection=True,
         use_jsonb=True
     )
